d04920b166b4b9e9de46f45b250ce8e556c7fc6a
user11111124 <daveman17.laptop@gmail.com>
2025-01-07 14:54:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -26,8 +26,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6820461647"))
 PORT = os.environ.get("PORT", "8030")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://munkog:6mOVQ1GqWD5P30XE@cluster245.e2vmr.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://xseries:xseries@xcluster.tnxdm.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "XCluster")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002395014488"))
